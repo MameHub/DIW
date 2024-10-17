ee00d129cdabf89302b4de748ded0927d8472282
Álvaro Cañas <acaygo@gmail.com>
2024-10-17 13:55:58 +0200

    arco con canvas

---
+++ canvas_clase/canvas7.js
@@ -0,0 +1,19 @@
+function cargaContextoCanvas(idCanvas){
+    let elemento = document.getElementById(idCanvas);
+    if(elemento && elemento.getContext){
+        let contexto = elemento.getContext('2d');
+        if(contexto){
+            return contexto;
+        }
+    }
+    return false;
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    let contexto = cargaContextoCanvas("micanvas");
+    if(contexto) {
+        contexto.beginPath();
+        contexto.arc(75,75,60,Math.PI,Math.PI*0.5,false);
+        contexto.stroke();
+    }
+});
